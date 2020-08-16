6de8e76d3a2d1c3da821ec3d4444ebfaf357c73e
Tony Yang <superzoro168@gmail.com>
2020-08-16 12:12:54 +0800

    Use Coroutines instead of AsyncTask

--- app/src/main/java/tw/tonyyang/englishwords/FileChooserFragment.kt
+++ app/src/main/java/tw/tonyyang/englishwords/FileChooserFragment.kt
@@ -12,22 +12,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import jxl.Workbook
+import kotlinx.coroutines.*
+import org.greenrobot.eventbus.EventBus
 import org.slf4j.LoggerFactory
 import tw.tonyyang.englishwords.App.Companion.db
+import tw.tonyyang.englishwords.database.Word
 import tw.tonyyang.englishwords.databinding.FragmentDropboxchooserBinding
-import tw.tonyyang.englishwords.util.LoadTask
 import tw.tonyyang.englishwords.util.PermissionManager
 import tw.tonyyang.englishwords.util.PermissionManager.PermissionCallback
-import tw.tonyyang.englishwords.util.Tool
+import tw.tonyyang.englishwords.util.UiUtils
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
 
 class FileChooserFragment : Fragment() {
 
     private lateinit var binding: FragmentDropboxchooserBinding
+
+    private var fileUrl: String? = null
+
+    private val progress: AlertDialog? by lazy {
+        UiUtils.getProgressDialog(activity as? Context, activity?.getString(R.string.loading_message))
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentDropboxchooserBinding.inflate(inflater, container, false)
@@ -38,13 +47,58 @@ class FileChooserFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.btnSubmit.setOnClickListener {
             GlobalScope.launch(Dispatchers.Main) {
-                withContext(Dispatchers.Default) {
+                progress?.show()
+                withContext(Dispatchers.IO) {
                     db?.userDao()?.deleteAll()
-                }.let {
-                    val task = LoadTask(activity as Context)
-                    task.setShowProgressView(true)
-                    task.execute()
+                    var data = ByteArray(0)
+                    if (fileUrl?.contains("content://") == true || fileUrl?.contains("file:///") == true) {
+                        data = readFile(fileUrl)
+                    } else {
+                        try {
+                            val url = URL(fileUrl)
+                            val arrayOutputStream = ByteArrayOutputStream()
+                            val connection = url.openConnection() as? HttpURLConnection
+                            connection?.connectTimeout = 10 * 1000
+                            connection?.connect()
+                            if (connection?.responseCode == 200) {
+                                val inputStream = connection.inputStream
+                                val buffer = ByteArray(10 * 1024)
+                                while (true) {
+                                    val len = inputStream.read(buffer)
+                                    if (len == -1) {
+                                        break
+                                    }
+                                    arrayOutputStream.write(buffer, 0, len)
+                                }
+                                arrayOutputStream.close()
+                                inputStream.close()
+                                data = arrayOutputStream.toByteArray()
+                            }
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                    var fileOutputStream: FileOutputStream? = null
+                    try {
+                        fileOutputStream = activity?.openFileOutput(TMP_FILE_NAME, Context.MODE_PRIVATE)
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    }
+                    if (fileOutputStream != null) {
+                        try {
+                            fileOutputStream.write(data)
+                            fileOutputStream.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                    readExcel(activity as? Context)
                 }
+                progress?.dismiss()
+                Toast.makeText(context, activity?.getString(R.string.loading_complete), Toast.LENGTH_LONG).show()
+                val realTimeUpdateEvent = RealTimeUpdateEvent(RealTimeUpdateEvent.Type.UPDATE_WORD_LIST)
+                realTimeUpdateEvent.message = "更新列表資料"
+                EventBus.getDefault().post(realTimeUpdateEvent)
             }
         }
         binding.btnSubmit.isEnabled = false
@@ -71,6 +125,48 @@ class FileChooserFragment : Fragment() {
         }
     }
 
+    private fun readFile(filePath: String?): ByteArray {
+        val arrayOutputStream = ByteArrayOutputStream()
+        val uri = Uri.parse(filePath)
+        activity?.contentResolver?.openInputStream(uri)?.use {
+            val buffer = ByteArray(10 * 1024)
+            while (true) {
+                val len = it.read(buffer)
+                if (len == -1) {
+                    break
+                }
+                arrayOutputStream.write(buffer, 0, len)
+            }
+            arrayOutputStream.close()
+        }
+        return arrayOutputStream.toByteArray()
+    }
+
+    private suspend fun readExcel(context: Context?) {
+        if (context == null) {
+            return
+        }
+        // TODO: fix this warning
+        context.openFileInput(TMP_FILE_NAME).use {
+            val book = Workbook.getWorkbook(it)
+            book.numberOfSheets
+            val sheet = book.getSheet(0)
+            val rows = sheet.rows
+            for (i in 0 until rows) {
+                if (sheet.getCell(0, i).contents[0].toString() == "#") continue
+                val word = Word(
+                        word = sheet.getCell(0, i).contents,
+                        wordMean = sheet.getCell(1, i).contents,
+                        category = sheet.getCell(2, i).contents,
+                        wordStar = sheet.getCell(3, i).contents,
+                        wordSentence = sheet.getCell(4, i).contents
+                )
+                db?.userDao()?.insertAll(word)
+            }
+            book.close()
+        }
+    }
+
     private fun chooseFileFromLocal() {
         val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
             type = "application/vnd.ms-excel"
@@ -89,8 +185,8 @@ class FileChooserFragment : Fragment() {
                 logger.debug("file chooser")
                 val uri = data?.data
                 if (uri != null) {
-                    Tool.fileUrl = uri.toString()
-                    binding.tvFilename.text = Tool.fileUrl
+                    fileUrl = uri.toString()
+                    binding.tvFilename.text = fileUrl
                     binding.tvFileSize.text = ""
                     binding.btnSubmit.isEnabled = true
                 }
@@ -111,5 +207,6 @@ class FileChooserFragment : Fragment() {
 
     companion object {
         private val logger = LoggerFactory.getLogger(FileChooserFragment::class.java)
+        private const val TMP_FILE_NAME = "vocabulary.xls"
     }
 }
--- app/src/main/java/tw/tonyyang/englishwords/util/UiUtils.kt
+++ app/src/main/java/tw/tonyyang/englishwords/util/UiUtils.kt
@@ -13,7 +13,7 @@ import androidx.core.content.ContextCompat
 class UiUtils {
 
     companion object {
-        fun getProgressDialog(context: Context, message: String): AlertDialog {
+        fun getProgressDialog(context: Context?, message: String?): AlertDialog? {
             val llPadding = 30
             val ll = LinearLayout(context).apply {
                 orientation = LinearLayout.HORIZONTAL
@@ -34,23 +34,25 @@ class UiUtils {
             }
 
             llParam = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT)
-            llParam.gravity = Gravity.CENTER
+                    ViewGroup.LayoutParams.WRAP_CONTENT).apply {
+                llParam.gravity = Gravity.CENTER
+            }
             val tvText = TextView(context)
             tvText.text = message
-            tvText.setTextColor(ContextCompat.getColor(context, android.R.color.black))
+            context?.let { tvText.setTextColor(ContextCompat.getColor(it, android.R.color.black)) }
             tvText.textSize = 20.toFloat()
             tvText.layoutParams = llParam
 
             ll.addView(progressBar)
             ll.addView(tvText)
 
-            val builder = AlertDialog.Builder(context)
-            builder.setCancelable(true)
-            builder.setView(ll)
+            val builder = context?.let { AlertDialog.Builder(it) }?.apply {
+                setCancelable(true)
+                setView(ll)
+            }
 
-            val dialog = builder.create()
-            val window = dialog.window
+            val dialog = builder?.create()
+            val window = dialog?.window
             if (window != null) {
                 val layoutParams = WindowManager.LayoutParams()
                 layoutParams.copyFrom(dialog.window?.attributes)
