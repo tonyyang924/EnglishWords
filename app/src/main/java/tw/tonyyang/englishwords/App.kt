70cc146667e6c83dcfc532adb17b9688759ff003
Tony Yang <superzoro168@gmail.com>
2020-10-17 18:39:06 +0800

    Remove google/firebase sdk and bump version

--- app/src/main/java/tw/tonyyang/englishwords/App.kt
+++ app/src/main/java/tw/tonyyang/englishwords/App.kt
@@ -1,10 +1,7 @@
 package tw.tonyyang.englishwords
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
 import com.facebook.stetho.Stetho
-import com.google.firebase.analytics.FirebaseAnalytics
-import io.fabric.sdk.android.Fabric
 import tw.tonyyang.englishwords.database.AppDatabase
 import tw.tonyyang.englishwords.database.AppDatabase.Companion.getDatabase
 
@@ -12,12 +9,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val fabric = Fabric.Builder(this)
-                .kits(Crashlytics())
-                .debuggable(true)
-                .build()
-        Fabric.with(fabric)
-        FirebaseAnalytics.getInstance(this)
         Stetho.initializeWithDefaults(this)
         db = getDatabase(this)
     }
