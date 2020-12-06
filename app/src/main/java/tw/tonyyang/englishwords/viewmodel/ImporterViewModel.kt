66add1daea34857c67275443060f4d5131f80f05
Tony Yang <superzoro168@gmail.com>
2020-12-06 23:02:25 +0800

    Use Flow instead of Coroutine

--- app/src/main/java/tw/tonyyang/englishwords/viewmodel/ImporterViewModel.kt
+++ app/src/main/java/tw/tonyyang/englishwords/viewmodel/ImporterViewModel.kt
@@ -1,13 +1,16 @@
 package tw.tonyyang.englishwords.viewmodel
 
 import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import tw.tonyyang.englishwords.App
 import tw.tonyyang.englishwords.Logger
 import tw.tonyyang.englishwords.R
 import tw.tonyyang.englishwords.data.ImporterResult
 import tw.tonyyang.englishwords.repository.ExcelRepository
-import java.lang.Exception
 import kotlin.system.measureTimeMillis
 
 class ImporterViewModel(
@@ -26,19 +29,17 @@ class ImporterViewModel(
         viewModelScope.launch {
             _isLoading.value = true
             val spendTime = measureTimeMillis {
-                val wordList = try {
-                    excelRepository.getWordList(fileUrl)
-                } catch (e: Exception) {
-                    _showResult.value = ImporterResult.Error(e)
-                    return@measureTimeMillis
-                }
-                if (wordList.isEmpty()) {
-                    _showResult.value = ImporterResult.Failure(App.appContext.getString(R.string.import_excel_failed_word_list_empty))
-                    return@measureTimeMillis
-                }
-                App.db?.userDao()?.deleteAll()
-                App.db?.userDao()?.insertAll(wordList)
-                _showResult.value = ImporterResult.Success(App.appContext.getString(R.string.import_excel_complete))
+                excelRepository.getWordList(fileUrl)
+                        .flowOn(Dispatchers.IO)
+                        .catch { e -> _showResult.value = ImporterResult.Error(e) }
+                        .collect { wordList ->
+                            if (wordList.isEmpty()) {
+                                _showResult.value = ImporterResult.Failure(App.appContext.getString(R.string.import_excel_failed_word_list_empty))
+                            }
+                            App.db?.userDao()?.deleteAll()
+                            App.db?.userDao()?.insertAll(wordList)
+                            _showResult.value = ImporterResult.Success(App.appContext.getString(R.string.import_excel_complete))
+                        }
             }
             Logger.d(TAG, "spendTime: $spendTime ms")
             _isLoading.value = false
