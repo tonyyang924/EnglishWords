b22c2aa97fbdc45f83d2f5a5de7d1eddc31e84b6
Tony Yang <superzoro168@gmail.com>
2021-06-05 22:16:01 +0800

    refactor: change data to data class

--- app/src/main/java/tw/tonyyang/englishwords/database/entity/Word.kt
+++ app/src/main/java/tw/tonyyang/englishwords/database/entity/Word.kt
@@ -6,21 +6,11 @@ import androidx.room.PrimaryKey
 import java.io.Serializable
 
 @Entity
-class Word(@PrimaryKey(autoGenerate = true) val id: Int = 0,
-           @ColumnInfo(name = "category") val category: String,
-           @ColumnInfo(name = "word") val word: String,
-           @ColumnInfo(name = "word_star") val wordStar: String,
-           @ColumnInfo(name = "word_mean") val wordMean: String,
-           @ColumnInfo(name = "word_sentence") val wordSentence: String
-) : Serializable {
-    override fun toString(): String {
-        return "Words{" +
-                "id='" + id + '\'' +
-                ", category='" + category + '\'' +
-                ", word='" + word + '\'' +
-                ", wordStar='" + wordStar + '\'' +
-                ", wordMean='" + wordMean + '\'' +
-                ", wordSentence='" + wordSentence + '\'' +
-                '}'
-    }
-}
+data class Word(
+        @PrimaryKey(autoGenerate = true) val id: Int = 0,
+        @ColumnInfo(name = "category") val category: String,
+        @ColumnInfo(name = "word") val word: String,
+        @ColumnInfo(name = "word_star") val wordStar: String,
+        @ColumnInfo(name = "word_mean") val wordMean: String,
+        @ColumnInfo(name = "word_sentence") val wordSentence: String
+) : Serializable
